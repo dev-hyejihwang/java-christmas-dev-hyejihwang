19649434cfa02494efee57320c32350bebc53b14
dev.hyejihwang@gmail.com <dev.hyejihwang@gmail.com>
2023-11-12 11:44:24 +0900

    Date 클래스 생성
    방문 일자 검증 클래스 생성 및 구현 함수 기능 정리

---
+++ src/main/java/christmas/domain/biz/Date.java
@@ -0,0 +1,22 @@
+package christmas.domain.biz;
+
+public class Date {
+
+//- [] 방문 일자를 검증 validateDate()
+//    - [] 1~31 까지만 입력 가능하며 이외 값 입력시 "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." 메시지 출력
+//- [] 고객 입력 날짜를 재확인 요청 confirmDate()
+//- [] 고객 입력 날짜에 진행하는 이벤트 안내 메시지 출력 notifyEvent()
+
+    public void validateDate(){
+
+    }
+
+    public void confirmDate(){
+
+    }
+
+    public void notifyEvent(){
+
+    }
+
+}
