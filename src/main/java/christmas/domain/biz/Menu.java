59773eeab2d8b370cba4530652d011819957530f
dev.hyejihwang@gmail.com <dev.hyejihwang@gmail.com>
2023-11-12 22:33:56 +0900

    Menu 클래스 생성
    입력 메뉴 관련 검증 클래스 생성 및 구현 함수 기능 정리

---
+++ src/main/java/christmas/domain/biz/Menu.java
@@ -0,0 +1,17 @@
+package christmas.domain.biz;
+
+public class Menu {
+
+    public void validateForm(){
+        //TODO 주문 형식 검증
+    }
+
+    public void validateMenu(){
+        //TODO 주문 메뉴 검증
+    }
+
+    public void validateMenuCount(){
+        //TODO 주문 메뉴 수량 검증
+    }
+     
+}
