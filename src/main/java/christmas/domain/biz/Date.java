9cf413d05d1af3f59c7d98f3ffa174782fb0a7da
dev.hyejihwang@gmail.com <dev.hyejihwang@gmail.com>
2023-11-12 12:43:02 +0900

    OutputView 클래스 생성
    고객 입력 날짜 진행 안내 메시지 출력 클래스 변경 Date -> OutputView
    기능 목록 수정

--- src/main/java/christmas/domain/biz/Date.java
+++ src/main/java/christmas/domain/biz/Date.java
@@ -7,7 +7,6 @@ public class Date {
 //    - [X] 1~31 까지만 입력 가능하며 이외 값 입력시 "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." 메시지 출력
 //- [X] 고객 입력 날짜를 재확인 요청 confirmDate()
 //    - [] 날짜 변경 요청 시 날짜 입력으로 리턴
-//- [] 고객 입력 날짜에 진행하는 이벤트 안내 메시지 출력 notifyEvent()
 
     public int validateDate(String inputDate) {
         try{
@@ -27,10 +26,6 @@ public class Date {
         System.out.println("방문 날짜는 " + visitDate + "일 입니다." );
 
         // 추후 날짜 변경 가능 로직 구현
-    }
-
-    public void notifyEvent(){
-
     }
 
 }
---
+++ src/main/java/christmas/domain/ui/OutputView.java
@@ -0,0 +1,9 @@
+package christmas.domain.ui;
+
+public class OutputView {
+
+    //- [] 고객 입력 날짜에 진행하는 이벤트 안내 메시지 출력 notifyEvent()
+    public void notifyEvent(int visitDate){
+
+    }
+}
