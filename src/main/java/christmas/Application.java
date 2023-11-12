27a1cb4ec67232b59ae853f9058cc6f502827170
dev.hyejihwang@gmail.com <dev.hyejihwang@gmail.com>
2023-11-12 11:36:30 +0900

    InputView.readDate 생성
    식당 방문 일자 입력 함수 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,14 @@
 package christmas;
 
+import christmas.domain.ui.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+
+        InputView inputView = new InputView();
+        int inputDate = inputView.readDate();
+        System.out.println("입력받은 날짜:" +inputDate);
+
     }
 }
